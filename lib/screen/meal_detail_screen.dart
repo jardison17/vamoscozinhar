c325f16f7c6dcd6d7859223a9453cd20017fa27a
jardison17 <jallima1997@gmail.com>
2025-11-15 18:18:47 -0300

    new

--- lib/screen/meal_detail_screen.dart
+++ lib/screen/meal_detail_screen.dart
@@ -2,6 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:myapp/models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
+  Widget _createSelecTitle(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(title, style: Theme.of(context).textTheme.titleLarge),
+    );
+  }
+
+  Widget _createSelectContainer(Widget child) {
+    return Container(
+      width: 300,
+      height: 200,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)!.settings.arguments as Meal;
@@ -19,22 +42,20 @@ class MealDetailScreen extends StatelessWidget {
             width: double.infinity,
             child: Image.network(meal.imageUrl, fit: BoxFit.cover),
           ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredientes',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-          ),
-          Container(
-            width: 200,
-            height: 300,
-            child: ListView.builder(
+          _createSelecTitle(context, 'Ingredientes'),
+          _createSelectContainer(
+            ListView.builder(
               itemCount: meal.ingredients.length,
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Text(meal.ingredients[index]),
-                  color: Colors.lightGreen,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                  color: Colors.orange,
                 );
               },
             ),
